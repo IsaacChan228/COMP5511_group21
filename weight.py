76b4279b170c052a559d575c7a67b460327734f2
Chan Lok Hay <24014193G@connect.polyu.hk>
2025-04-15 11:51:44 +0000

    updated weight

--- weight.py
+++ weight.py
@@ -3,41 +3,43 @@
 ### center row preference ###
 
 # occupying the center col
-center_col_merit = 10
+center_col_merit = 5
 
 # occupying the column adjacent to the center col
-adj_col_merit = 5
+adj_col_merit = 3
 
 
 ### 4-cell window evaluation ###
  
 # 4-piece: Winning move
 # Highest priority, guarantees a win
-winning_move_4p = 1000000
+winning_move_4p = 6000
 
 # 3-piece: 1 move away from winning
 # Single 3-piece-in-a-row, important for progress
-winning_move_3p = 300000
+winning_move_3p = 250
 
 # 2-piece: 2 moves away from winning
 # Single 2-piece-in-a-row, lowest priority
-winning_move_2p = 100000
+winning_move_2p = 50
 
 # 3 opponent piece: 1 move away from losing
 # Second highest priority, prevents losing
-losing_move_3p = 900000
+losing_move_3p = 5500
 
 
 ### multiple winning pattern bonus ###
 
 # 2 or more 4-piece: Winning move
 # Multiple 4-piece-in-a-row, still guarantees a win
-m_winning_move_4p = 800000
+# single 4-piece-in-a-row already guarantees a win, so no extra bonus
+m_winning_move_4p = 0
 
 # 2 or more 3-piece: 1 move away from winning
 # Multiple 3-piece-in-a-row, very close to winning
-m_winning_move_3p = 500000
+# This do not check if the multiple 3-piece-in-a-row two-ended, which is a winning move
+m_winning_move_3p = 2500
 
 # 2 or more 2-piece: 2 moves away from winning
 # Multiple 2-piece-in-a-row, still important
-m_winning_move_2p = 10000
+m_winning_move_2p = 200
